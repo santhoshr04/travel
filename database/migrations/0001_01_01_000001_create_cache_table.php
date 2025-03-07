ee8ba7f335dfe6fb1321c3eea12d048105c13769
Santhoshr04 <santhoshr0415@gmail.com>
2025-03-07 09:13:42 +0530

    reslovemerge confilts in migration

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('cache', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->mediumText('value');
-            $table->integer('expiration');
+            $table->integer('key');
         });
 
         Schema::create('cache_locks', function (Blueprint $table) {
