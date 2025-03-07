06efbfa96d9036a8f1158b077fb29144449f7a71
Santhoshr04 <santhoshr0415@gmail.com>
2025-03-07 09:36:55 +0530

    expiration

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('cache', function (Blueprint $table) {
-            $table->string('key')->primary();
-            $table->mediumText('value');
-            $table->integer('key');
+            $table->string('expiration')->primary();
+            $table->mediumText('expiration');
+            $table->integer('expiration');
         });
 
         Schema::create('cache_locks', function (Blueprint $table) {
